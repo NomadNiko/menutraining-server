004d7355043eda2dad498a87df69f83f512658a9
NomadNiko <nomad.niko.inc@gmail.com>
2025-03-15 17:33:11 -1000

    fix: quickfix

--- src/files/config/file.config.ts
+++ src/files/config/file.config.ts
@@ -1,39 +1,45 @@
 import { registerAs } from '@nestjs/config';
-import { IsEnum, IsString, IsBoolean, ValidateIf } from 'class-validator';
+import { IsEnum, IsString, IsOptional, ValidateIf } from 'class-validator';
 import validateConfig from '../../utils/validate-config';
 import { FileDriver, FileConfig } from './file-config.type';
 
 class EnvironmentVariablesValidator {
   @IsEnum(FileDriver)
+  @IsOptional()
   FILE_DRIVER: FileDriver;
 
   @ValidateIf((envValues) =>
     [FileDriver.S3, FileDriver.S3_PRESIGNED].includes(envValues.FILE_DRIVER),
   )
   @IsString()
+  @IsOptional()
   ACCESS_KEY_ID: string;
 
   @ValidateIf((envValues) =>
     [FileDriver.S3, FileDriver.S3_PRESIGNED].includes(envValues.FILE_DRIVER),
   )
   @IsString()
+  @IsOptional()
   SECRET_ACCESS_KEY: string;
 
   @ValidateIf((envValues) =>
     [FileDriver.S3, FileDriver.S3_PRESIGNED].includes(envValues.FILE_DRIVER),
   )
   @IsString()
+  @IsOptional()
   AWS_DEFAULT_S3_BUCKET: string;
 
   @ValidateIf((envValues) =>
     [FileDriver.S3, FileDriver.S3_PRESIGNED].includes(envValues.FILE_DRIVER),
   )
   @IsString()
+  @IsOptional()
   AWS_S3_REGION: string;
 
   @ValidateIf((envValues) => [FileDriver.S3].includes(envValues.FILE_DRIVER))
-  @IsBoolean()
-  AWS_S3_PUBLIC_ACCESS: boolean;
+  @IsString()
+  @IsOptional()
+  AWS_S3_PUBLIC_ACCESS: string; // Changed from IsBoolean to IsString with optional
 }
 
 export default registerAs<FileConfig>('file', () => {
@@ -45,8 +51,8 @@ export default registerAs<FileConfig>('file', () => {
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
     awsDefaultS3Bucket: process.env.AWS_DEFAULT_S3_BUCKET,
     awsS3Region: process.env.AWS_S3_REGION,
-    maxFileSize: 52428800, // 50mb
-    // Add the public access flag
+    maxFileSize: 5242880, // 5mb
+    // Convert the string to boolean
     awsS3PublicAccess: process.env.AWS_S3_PUBLIC_ACCESS === 'true',
   };
 });
